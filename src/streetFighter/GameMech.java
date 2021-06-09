0ac600290cf9c3f9778aa7dfffd76bbbfa550500
Code Cadet <joaomanuelsilvapereira@hotmail.com>
2021-06-09 14:25:30 +0100

    basic GameMech functionality

--- src/streetFighter/GameMech.java
+++ src/streetFighter/GameMech.java
@@ -1,4 +1,15 @@
 package streetFighter;
 
 public class GameMech {
+
+    boolean mechRunning = true;
+
+    public void mechInit() {
+
+        while (mechRunning) {
+            System.out.println("do the stuffs");
+        }
+
+    }
+
 }
