b7816eda5ab8050c9cc0d5e85b1eac67e1be303d
Rui Cunha <ruimiguelrcacunha@gmail.com>
2021-06-10 13:28:29 +0100

    something on main

--- src/streetFighter/Main.java
+++ src/streetFighter/Main.java
@@ -7,13 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-       streetFighter.Game game = new streetFighter.Game();
+        streetFighter.Game game = new streetFighter.Game();
 
-        //game.init();
-         //game.start();
-        new Inputs();
-        new ChooseFighter();
-
-
+        game.init();
+        //game.start();
+//        new Inputs();
+//        new ChooseFighter();
+//
+//
+//    }
     }
 }
